03ce90645848ef02cadf072b0186d1d60e407ec6
tranduc0205 <ducco6655@gmail.com>
2025-03-13 22:43:32 +0700

    Add files via upload

---
+++ bài 2/bài2.11.py
@@ -0,0 +1,10 @@
+l= [1, "python",4,7]
+k=["cse",2,"guntur",8]
+m=[]
+m.append(l)
+m.append(k)
+print(m)
+d={1:1,2:k,'combine list':m}
+print(d)
+print("sv:tran nguyen viet duc")
+print("235752020710013")
