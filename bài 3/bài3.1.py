8ad81618b05c5f236f9a36b3ad8c79c4628bd586
tranduc0205 <ducco6655@gmail.com>
2025-03-14 10:14:45 +0700

    Add files via upload

---
+++ bài 3/bài3.1.py
@@ -0,0 +1,9 @@
+def sum (a,b):
+    print("sum=" + str(a+b))
+    # (a+b) cho ra kết quả là kiểu số nguyên:int
+    #str(a+b) cho ra kết quả chuyển từ số nguyên sang chuỗi
+    # dấu + để nối hay chuỗi lại với nhau
+sum(4,5)
+sum(3,7)
+print("tran nguyen viet duc")
+print("mssv")
