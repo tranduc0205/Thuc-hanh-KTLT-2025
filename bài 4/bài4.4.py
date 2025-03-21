4b07cdc7be976329a65c4462d6f80bd44fe3d29c
tranduc0205 <ducco6655@gmail.com>
2025-03-21 11:00:29 +0700

    Add files via upload

---
+++ bài 4/bài4.4.py
@@ -0,0 +1,7 @@
+ds = input("danh sach:").split( )
+# split: tách chuỗi thành các chuỗi nhỏ dựa trên khoảng trắng
+print(ds)
+
+for so in ds :
+    # lặp từng chuỗi nhỏ trong ds
+    print(so)   # in lần lượt các chuỗi nhỏ ra màn hình
