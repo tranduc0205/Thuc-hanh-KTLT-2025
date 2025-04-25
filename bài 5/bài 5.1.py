fa6b66d8cc8e1fc140e21f1778fbf1dbe012de97
tranduc0205 <ducco6655@gmail.com>
2025-04-25 08:16:34 +0700

    Add files via upload

---
+++ bài 5/bài 5.1.py
@@ -0,0 +1,13 @@
+Print("sv: trần nguyễn viết đức.")
+print ("mssv:235752020710013")
+
+import nháp # Note no.py
+
+values= [2,4,6,8,10]
+print('Squares:')
+for v in values:
+    print (nháp.square(v))
+print("Cubes:")
+for v in values:
+    print(nháp.cube(v))
+print('Average:'+ str(nháp.average(values)))
