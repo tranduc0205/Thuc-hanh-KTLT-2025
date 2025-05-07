dd27ff400e8acab4237ba95c7fc570c40074cc0a
tranduc0205 <ducco6655@gmail.com>
2025-05-07 21:47:57 +0700

    Add files via upload

---
+++ Bài 2/bài 2.3.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number--->"))
+if n % 2 == 0:
+    print("EVEN Number")
+else:
+    print("ODD Number")
+print("trần nguyễn viết đức")
+print("mssv:23575205020710013")
---
+++ Bài 2/bài 2.5.py
@@ -0,0 +1,6 @@
+n = int(input("Enter A Number : "))
+while n>=0:
+    print(n)
+    n=n-1
+print("sv:tran nguyen viet duc")
+print("235752020710013")
