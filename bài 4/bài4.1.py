c231302a36e3faf8fd52d124df28169d0955e3bb
tranduc0205 <ducco6655@gmail.com>
2025-05-08 22:12:49 +0700

    Delete bài 4/bài4.1.py

--- bài 4/bài4.1.py
+++
@@ -1,7 +0,0 @@
-s= input(" nhập chuỗi: ")
-# ch là một biến tạm thời lưu các giá trị ký tự của chuỗi s qua mỗi lần lặp
-for ch in s :
-    print(ch)
-    # in ra từng ký tự mà biến "ch" nhận được qua mỗi vòng lặp, kể cả dấu cách
-print("tran nguyen viet duc")
-print("mssv:23575205020710013")
