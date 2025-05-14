f459e0f5060884667d31fbd34bf17b6c83d7ae59
tranduc0205 <ducco6655@gmail.com>
2025-05-14 22:18:08 +0700

    Update bài_4.20.py

--- Bài 4/bài_4.20.py
+++ Bài 4/bài_4.20.py
@@ -2,12 +2,16 @@ import math
 
 # Hàm tính tổ hợp C(n, k)
 def to_hop(n, k):
-    return math.comb(n, k)  # dùng từ Python 3.8 trở lên
+    return math.comb(n, k)  # Yêu cầu Python 3.8+
 
-# Nhập n từ người dùng
+# Nhập số dòng từ người dùng
 n = int(input("Nhập số dòng n: "))
 
-# In dòng thứ n của tam giác Pascal
-dong = [to_hop(n, k) for k in range(n + 1)]
-print("Dòng thứ", n, "của tam giác Pascal là:")
-print(dong)
+# In n dòng đầu tiên của tam giác Pascal
+print(f"{n} dòng đầu tiên của tam giác Pascal:")
+for i in range(n):
+    dong = [to_hop(i, k) for k in range(i + 1)]
+    print(dong)
+
+print("trần nguyễn viết đức")
+print("mssv:23575205020710013")
