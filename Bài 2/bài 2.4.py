566f5e49416d0a4e98adeb8cb02a3e349ccd3ba0
tranduc0205 <ducco6655@gmail.com>
2025-05-14 16:04:42 +0700

    Update bài 2.4.py

--- Bài 2/bài 2.4.py
+++ Bài 2/bài 2.4.py
@@ -1,5 +1,8 @@
-a= []
-for i in range ( 2000, 3201):
- if (i % 7==0) and (i%5!=0):
-    a.append(str(i))
-print(",".join(a))
+print("Trần Nguyễn Viết Đức")
+print("235752020710013")
+############################################
+i = 1
+for j in range(2, 10):
+    print("i:", i, "j:", j)
+    print(i, "/", j)
+    print(i / j)
